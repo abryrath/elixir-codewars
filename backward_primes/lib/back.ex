831632e71a4cb35e59d96dfb5121e2b8c47c8889
Abry Rath <abryrath@gmail.com>
2019-12-01 12:34:59 -0500

    more solutions

---
+++ backward_primes/lib/back.ex
@@ -0,0 +1,18 @@
+defmodule Back do
+  @moduledoc """
+  Documentation for Back.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Back.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
